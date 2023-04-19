73488f6306567e8bcb13b1e1b63d1a53fe160573
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-19 15:49:14 +0300

    Getaxis, RigidBody
    Getaxis for steerSpeed & movement of the car
    Rigidbody to add bumping into funcitonality

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 2f;
-    [SerializeField] float moveSpeed = 0.04f;
+    [SerializeField] float steerSpeed = 200;
+    [SerializeField] float moveSpeed = 20;
 
 
     // Start is called before the first frame update
@@ -17,8 +17,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steeramount = Input.GetAxis("Horizontal") * steerSpeed;
-        float moveamount = Input.GetAxis("Vertical") * moveSpeed;
+        float steeramount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveamount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0, -steeramount);
         transform.Translate(0,moveamount,0);
     }
