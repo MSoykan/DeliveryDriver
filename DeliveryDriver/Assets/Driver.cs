9e6c9f349f294365f15a4b447467f18621aefe7e
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-19 15:27:52 +0300

    added getaxis functions to steering and speed

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 2f;
+    [SerializeField] float moveSpeed = 0.04f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0,0,45);
+        //transform.Rotate(0,0,45);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steeramount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveamount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0,0, -steeramount);
+        transform.Translate(0,moveamount,0);
     }
 }
