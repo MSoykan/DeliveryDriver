8de390cdda7ef48a05701441985527d2c00822e3
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-24 16:30:56 +0300

    moved gitignore to root

--- DeliveryDriver/Assets/Collision.cs
+++ DeliveryDriver/Assets/Collision.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Colliding with sth!!");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered. kek.");
     }
 }
