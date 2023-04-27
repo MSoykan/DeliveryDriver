293f9f88a7b43dc9f3c1f333dc5cd0daa6d13ddf
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-27 12:50:00 +0300

    Boosts & Bump added

--- DeliveryDriver/Assets/Delivery.cs
+++ DeliveryDriver/Assets/Delivery.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
-{
+{   
+        [SerializeField] Color32 hasPackageColor = new Color32 ( 1, 1, 1, 1);
+        [SerializeField] Color32 deliverPackageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Colliding with sth!!");
     }
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag== "Package")
+        if(other.tag== "Package" && !hasPackage)
         {
             Debug.Log("Package picked up.");
             hasPackage=true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, 0.5f);
         }
 
         if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
+            hasPackage=false;
+            spriteRenderer.color= deliverPackageColor;
         }
     }
 }
--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -6,6 +6,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 200;
     [SerializeField] float moveSpeed = 20;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
 
     // Start is called before the first frame update
@@ -21,5 +23,15 @@ public class Driver : MonoBehaviour
         float moveamount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0, -steeramount);
         transform.Translate(0,moveamount,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Boost"){
+            moveSpeed=boostSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed=slowSpeed;
     }
 }
