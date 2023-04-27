10c52c95bbb98c6e3ff3cea8154cc4d19c20ff54
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-27 12:56:09 +0300

    WrapUpProject

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -11,10 +11,6 @@ public class Driver : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        //transform.Rotate(0,0,45);
-    }
 
     // Update is called once per frame
     void Update()
